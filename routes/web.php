d88a9b6fbc02939120ce12d98d2fc91c948ef630
vu.huy.tuan <vu.huy.tuan@FRAMGIA.COM>
2017-03-10 16:47:25 +0700

    Route in laravel 5

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('user/profile', [
+	'as' => 'profile',
+	'uses' => 'UserController@showProfile'
+	]);
+// tham số bắt buộc
+Route::get('user/{id}', function ($id) {
+	return 'User '.$id;
+});
+// định nghĩa nhiều tham số
+
+// dấu hỏi sau tham số: có nghĩa là tham số đó có thể có hoặc k. (không bắt buộc)
+Route::get('posts/{post}/comments/{comment?}', function ($postId, $commentId=null) {
+    return $postId .'='. $commentId;
+});
+// Ràng buộc
+Route::get('name/{name}', function ($name) {
+	return 'Name = '.$name;
+})->where('name', '[A-Za-z]+');
+Route::get('userid/{id}', function ($id) {
+	return 'ID= '. $id;
+}); //->where('id', '[0-9]+');
+
+//route group
+
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('users', function ()    {
+        // Matches The "/admin/users" URL
+        return 'prefix_admin';
+    });
+});
