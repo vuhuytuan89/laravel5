09b538f616feb824f233d7808e432c4d0681f520
vu.huy.tuan <vu.huy.tuan@FRAMGIA.COM>
2017-03-27 08:59:13 +0700

    update layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,7 +33,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-3">
-					@include("layouts.elements.slidebar")
+					@include("layouts.elements.sidebar")
 				</div>
 				<div class="col-sm-9 padding-right">
 					@yield('content')
